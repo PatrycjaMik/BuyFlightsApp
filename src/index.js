746b4b6658ac1a416d098da4e10b9fb7b9064367
Patrycja Mikula <patrycja1.mikula@gmail.com>
2023-03-05 12:33:35 +0100

    selects added

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import { sum2 } from "./sum2";
 // import Icon from "./assets/img/proba.png";
 
 console.log("Hello pati");
+const flightsFrom = document.getElementById("flightsFrom");
+const flightsTo = document.getElementById("flightsTo");
+const OptionValue = document.getElementById("flightsFrom").replaceChild;
+console.log(OptionValue);
+
+// function Check(option1, option2) {}
+//   flightsTo.innerHTML += `<option value=${el.id}> ${el.name} </option>`;
 
 fetch(
   "https://raw.githubusercontent.com/PatrycjaMik/BuyFlightsApp/main/db.json"
 )
   .then((resp) => resp.json()) // Transform the data into json
   .then(function (data) {
-    console.log(data.data);
+    data.data.forEach(function (el) {
+      flightsFrom.innerHTML += `<option id=${el.id}> ${el.name} </option>`;
+      flightsTo.innerHTML += `<option id=${el.id}> ${el.name} </option>`;
+    });
   });
+
+document.getElementById("flightsFrom").addEventListener("change", function () {
+  console.log("You selected: ", this.value);
+});
+
+document.getElementById("flightsTo").addEventListener("change", function () {
+  console.log("You selected: ", this.value);
+});
+
+///trash
 
 let heading = document.querySelector("#demo"),
   sumValue = sum(10, 5);
